3f1ec423999af1741b1daae7761b680ebc3b58e3
AdelMdh <91626632+AdelMdh@users.noreply.github.com>
2022-05-02 10:11:49 +0100

    mon premier test de création de projet avec vue cli et vue router

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+      elit. Obcaecati voluptates soluta consequuntur iure 
+      eius dignissimos sed modi ipsa quisquam maxime possimus
+       natus incidunt numquam quibusdam amet ex, corrupti voluptatem quaerat!</p>
   </div>
 </template>
---
+++ src/views/categorieView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="cards">
+        <div class="card">
+            <img src="/src/assets/imageCat/anime-05.png" alt="">
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    </div>
+</template>
